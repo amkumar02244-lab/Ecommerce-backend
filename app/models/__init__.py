c0c2f3bef58b65f0e48a72db3f7a2e55b23a3af6
Aman <amankumar72091@gmail.com>
2026-06-19 13:44:38 +0530

    migration to postgres1

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,3 @@
-from app.models.user import User, USER_COLUMNS, USER_TABLE
-from app.models.product import Product, PRODUCT_COLUMNS, PRODUCT_TABLE
-from app.models.order import (
-    CartItem, CART_COLUMNS, CART_TABLE,
-    Order, ORDER_COLUMNS, ORDER_TABLE,
-    OrderItem, ORDER_ITEM_COLUMNS, ORDER_ITEM_TABLE
-)
+from app.models.user import User
+from app.models.product import Product
+from app.models.order import CartItem, Order, OrderItem
